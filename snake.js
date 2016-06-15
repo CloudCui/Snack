449def5ea53cc8a7533fcd4464b43f231b622be4
Cloud Cui <cloud@Clouds-MacBook-Pro.local>
2016-06-15 21:54:15 +1000

    snake's able to move

--- snake.js
+++ snake.js
@@ -10,9 +10,9 @@ const left = 4;
 
 var started = false;
 var stopper;
-var nextDirection = 1;
-
-var snake = [];
+var nextStep = '9_11';
+var nextDirection = up;
+var snake = ['9_10', '9_9'];
 
 /**
  * Init after page is loaded
@@ -21,16 +21,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
     init();
 });
 
-/**
- * Use input to Draw grid
- */
-document.getElementById('submit').onclick = function (event) {
-    event.preventDefault();
-    var row = document.getElementById('row').value;
-    var col = document.getElementById('col').value;
-    // document.getElementById('table').innerHTML = createTable(row, col);
-    // document.getElementById('table').innerHTML = createTable(20, 20);
-};
+// CURRENTLY NOT BEING USED
+// ---------------------------------------------------------------------
+// /**
+//  * Use input to Draw grid
+//  */
+// document.getElementById('submit').onclick = function (event) {
+//     event.preventDefault();
+//     var row = document.getElementById('row').value;
+//     var col = document.getElementById('col').value;
+//     // document.getElementById('table').innerHTML = createTable(row, col);
+//     // document.getElementById('table').innerHTML = createTable(20, 20);
+// };
+// ---------------------------------------------------------------------
 
 /**
  * Start / Stop
@@ -53,7 +56,6 @@ document.getElementById('switch').onclick = function (event) {
  * Direction Listener
  */
 document.onkeydown = function (e) {
-    // console.log(e.keyCode);
     switch(e.keyCode) {
         case 37:
             nextDirection = left;
@@ -72,8 +74,6 @@ document.onkeydown = function (e) {
     }
 };
 
-
-
 /**
  * Init the grid, including:
  * - create the table
@@ -81,36 +81,9 @@ document.onkeydown = function (e) {
  */
 function init() {
     document.getElementById('table').innerHTML = createTable(20, 20);
-    
-    snake = ['9_10', '9_9'];
-
     render(snake);
-    // var cell;
-    for(i = 0; i < snake.length; i++) {
-        var pos = snake[i].split('_');
-        console.log(pos);
-        var snakePart = document.getElementById(pos[0] + '_' + pos[1]);
-        snakePart.classList.add('snakebody');
-    }
-    
-    x = 9;
-    y = 10;
-}
-
-function render(snake) {
 
 }
-
-function log() {
-    x++;
-    y++;
-    console.log(x + ' ' +  y);
-
-}
-function update() {
-
-}
-
 
 /**
  * Create the table
@@ -127,7 +100,7 @@ function createTable(row, col) {
 }
 
 /**
- * create the row 
+ * create the row
  */
 function createRow(r, col) {
     var row = '<tr>';
@@ -147,32 +120,99 @@ function createCell(y, x) {
 }
 
 
+
+
 /**
- * Move, the next direction before actual move depends on the global @var nextDirection
+ * Draw the snake. Add the 'snakebody' class to the snake
+ * This function does not care whether snake part needs to be removed
+ *
+ * @param s Snake
+ */
+function render(s) {
+    for(var i = 0; i < s.length; i++) {
+        var pos = s[i].split('_');
+        console.log(pos);
+        var sPart = document.getElementById(pos[0] + '_' + pos[1]);
+        sPart.classList.add('snakebody');
+    }
+}
+
+
+/**
+ * Move, the next direction before actual move depends on the global @var nextStep
  */
 function move() {
+
+    console.log('move --> ' + nextDirection);
+
+    var tmpPos = strToPos(snake[0]);
     switch(nextDirection) {
         case up:
-            y++;
+            tmpPos[1]++;
             break;
         case right:
-            x++;
+            tmpPos[0]++;
             break;
         case down:
-            y--;
+            tmpPos[1]--;
             break;
         case left:
-            x--;
+            tmpPos[0]--;
             break;
         default:
             break;
     }
 
-    var el = document.getElementById(x + '_' + y);
-    el.classList.add('snakebody');
+    nextStep = posToStr(tmpPos);
+
+
+    console.log(snake);
+
+
+    rebuildSnake();
+
+    // var el = document.getElementById(x + '_' + y);
+    // el.classList.add('snakebody');
+    // render(snake);
+}
+
+
+/**
+ * Add the new cell to snake and remove the last from snake
+ */
+function rebuildSnake() {
+    snake.unshift(nextStep);
+
+    var tail = document.getElementById(snake.pop());
+    tail.classList.remove('snakebody');
+
+
+    for(var i = 0; i < snake.length; i++) {
+        var el = document.getElementById(snake[i]);
+        el.classList.add('snakebody');
+    }
+}
+
+/**
+ * return first element in Snake
+ */
+function getSnakeHead() {
 
 }
 
-function render() {
+function getSnakeHead() {
+    return snake[0];
+}
 
+/**
+ * x is array[0], y is array[1]
+ * @param str
+ * @returns {Array}
+ */
+function strToPos(str) {
+    return str.split('_');
+}
+
+function posToStr(pos) {
+    return pos[0] + '_' + pos[1];
 }
